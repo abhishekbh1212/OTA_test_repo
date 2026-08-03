8a17a0aec6900863a4e0aa863a9b94f53aa3b59b
Your Name <abhishekbh@nexsemisystem.com>
2026-08-03 16:26:37 +0530

    Update Math App to v1.1.0 (5 + 5 = 10)

--- sum_ota.py
+++ sum_ota.py
@@ -1,13 +1,12 @@
 import time
-
 print("========================================")
-print("🚀 BINPRO OTA TEST APP — VERSION 1.0.0")
+print("🚀 BINPRO APP RUNNING — VERSION 1.1.0")
 print("========================================")
 
-num1 = 2
-num2 = 2
+num1 = 5
+num2 = 5
 result = num1 + num2
 
 while True:
-    print(f"[v1.0.0 Output] Math Result: {num1} + {num2} = {result}")
+    print(f"[v1.1.0 Output] Math Result: {num1} + {num2} = {result}")
     time.sleep(3)
