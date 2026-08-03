e03e53173a915bfbdf96964ea3600500bd1a2dbf
Your Name <abhishekbh@nexsemisystem.com>
2026-08-03 16:14:22 +0530

    intitial v1.0.0 math app and realeasee manifest

---
+++ sum_ota.py
@@ -0,0 +1,13 @@
+import time
+
+print("========================================")
+print("🚀 BINPRO OTA TEST APP — VERSION 1.0.0")
+print("========================================")
+
+num1 = 2
+num2 = 2
+result = num1 + num2
+
+while True:
+    print(f"[v1.0.0 Output] Math Result: {num1} + {num2} = {result}")
+    time.sleep(3)
