ad7b20edc4c855d699d015ec7abee44ca685919f
Your Name <abhishekbh@nexsemisystem.com>
2026-08-04 14:21:36 +0530

    Release v1.3.0 BinPro production update

---
+++ pro.py
@@ -0,0 +1,9 @@
+import time
+
+print("========================================")
+print("BinPro Smart Bin - VERSION 1.3.0")
+print("========================================")
+
+while True:
+    print("[v1.3.0] BinPro Production OTA Working Perfectly!")
+    time.sleep(3)
